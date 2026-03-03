a668de32934694b27ab54bd205c3c2e9023dfa69
Arya-025 <reyhan.arya.ft24@mail.umy.ac.id>
2026-03-03 15:26:11 +0700

    prosedur input

--- P2F1.cpp
+++ P2F1.cpp
@@ -4,3 +4,11 @@ using namespace std;
 // Deklarasi Global
 float p, l;
 
+// Implementasi Prosedur & Fungsi
+void Input() {
+    cout << "Masukkan Panjang : ";
+    cin >> p;
+    cout << "Masukkan Lebar : ";
+    cin >> l;
+}
+
